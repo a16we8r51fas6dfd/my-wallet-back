f48518378fbd348d5e90d24a1e0e3b63c387bb94
fernandinho <fernando_yudi_yaeda@hotmail.com>
2022-02-11 20:35:13 -0300

    fix: deploy

--- src/middlewares/validateSignUpSchemaMiddleware.js
+++ src/middlewares/validateSignUpSchemaMiddleware.js
@@ -3,11 +3,6 @@ import signUpSchema from "../schemas/signUpSchema.js";
 export default function validateSignUpSchema(req, res, next) {
     const validation = signUpSchema.validate(req.body)
 
-    if(req.body.password !== req.body.confirmPassword) {
-        res.sendStatus(409)
-        return
-    }
-
     if(validation.error) {
         res.sendStatus(422)
         return
